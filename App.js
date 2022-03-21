6b5c5ffc4ccbe46b46e0ce8ebc6529afd361fe65
BlackMarine <wkdeo0000@gmail.com>
2022-03-21 15:57:11 +0900

    2.2 expo-asset, @expo/vector-icons, try {} catch
    이미지 에셋 추가방법
    백터이미지 preload하기
    이미지가 없는경우 디버깅 방법 학습
    
    다음학습 : urility 함수이용 preload하는 더 나은 방법 알아보기

--- App.js
+++ App.js
@@ -1,13 +1,26 @@
 import AppLoading from "expo-app-loading";
 import React, { useState } from "react";
-import { Text } from "react-native"
+import * as Font from 'expo-font';
+import { Text, Image } from "react-native"
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from 'expo-asset';
+
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true); //로딩이 끝나면 호출해주는 함수
   const startLoading = async () => {
     //preLoad가 필요한 이유 : api호출하기, 어떤정보를 받아오기(notification정보 등등), 또는 DB를열고preload -> 어플켜면 바로접속가능, 또는 아이콘 로드하기
-    await new Promise((resolve) => setTimeout(resolve, 10000))
+    console.log("test");
+    await Font.loadAsync(Ionicons.font); 
+    try {
+      await Asset.loadAsync(require("./pop400_400.png"));
+    } catch (error) {
+      console.log(error);
+    }
+    await Image.prefetch("https://d33wubrfki0l68.cloudfront.net/b152eb4214943f96e83c4babde026b12221e68f1/a20c2/img/oss_logo.png");
+    await new Promise((resolve) => setTimeout(resolve, 3000))
+    
   }; //startAsync는 promise가 resolve되거나 종료됐을 때 promise를 반환해주는 함수
   if (!ready) {
     return (

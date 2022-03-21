e112ba04dc7fa5ec415140e6ef61f0ff8cdf82bb
BlackMarine <wkdeo0000@gmail.com>
2022-03-21 17:35:00 +0900

    syntex error fixd 이제 네비게이션 학습차례

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
   //앱로딩에서 preload만 하는것이라면 hook을 사용하면 3줄이면끝남
   //but, 로딩중에 db, api 등 뭔가를 추가할 수 있는데 추가작업이 있다면 조금 길지만 이전코드가 더 좋음.
   const [assets] = useAssets([require("./pop400_400.png")]);
-  const [loaded] = Font.useFonts([Ionicons.font]);
+  const [loaded] = Font.useFonts(Ionicons.font);
   if (!assets || !loaded) {
     return <AppLoading />;
   }

4e7001f392eb86a4e26a3a7ab394deadfd361f92
BlackMarine <wkdeo0000@gmail.com>
2022-03-21 17:10:43 +0900

    2.3 매개변수로 fonts배열을 받아서 [폰트,폰트 배열을 async.폰트,async.폰트..만들기]
    startLoading 을 사용하지 않는 방법을 hook을 이용해서 이제 알아보자

--- App.js
+++ App.js
@@ -5,23 +5,33 @@ import { Text, Image } from "react-native"
 import { Ionicons } from "@expo/vector-icons";
 import { Asset } from 'expo-asset';
 
+//매개변수로 fonts배열을 받아서 [폰트,폰트 배열을 async.폰트,async.폰트..만들기]
+const loadFonts = (fonts) => fonts.map(font => Font.loadAsync(font));
+
+const loadImages = (images) => 
+  images.map(image => {
+  if(typeof image === "string"){
+    return Image.prefetch(image)
+  } else {
+    return Asset.loadAsync(image)
+  }
+})
 
 export default function App() {
   const [ready, setReady] = useState(false);
   const onFinish = () => setReady(true); //로딩이 끝나면 호출해주는 함수
   const startLoading = async () => {
-    //preLoad가 필요한 이유 : api호출하기, 어떤정보를 받아오기(notification정보 등등), 또는 DB를열고preload -> 어플켜면 바로접속가능, 또는 아이콘 로드하기
-    console.log("test");
-    await Font.loadAsync(Ionicons.font); 
-    try {
-      await Asset.loadAsync(require("./pop400_400.png"));
-    } catch (error) {
-      console.log(error);
-    }
-    await Image.prefetch("https://d33wubrfki0l68.cloudfront.net/b152eb4214943f96e83c4babde026b12221e68f1/a20c2/img/oss_logo.png");
-    await new Promise((resolve) => setTimeout(resolve, 3000))
+    const fonts = loadFonts([Ionicons.font]);
+    //console.log(fonts);
+
+    const images = loadImages([require("./pop400_400.png"), "https://d33wubrfki0l68.cloudfront.net/b152eb4214943f96e83c4babde026b12221e68f1/a20c2/img/oss_logo.png"])
+    //console.log(images); 이제 fonts 배열과 images 배열을 가지고 큰 배열을 만듬 ... 이용
+    await Promise.all([...fonts, ...images]);
     
+
+    await new Promise((resolve) => setTimeout(resolve, 3000))
   }; //startAsync는 promise가 resolve되거나 종료됐을 때 promise를 반환해주는 함수
+
   if (!ready) {
     return (
       <AppLoading
@@ -31,5 +41,5 @@ export default function App() {
       />
     );
   }
-  return <Text>we are done loading</Text>;
+  return <Text>we are done loading!!!</Text>;
 }

c994dd48a9ed39bad0b36e3a074a08581dc49685
BlackMarine <wkdeo0000@gmail.com>
2022-03-23 16:45:27 +0900

    2.6 탭  options={{정리

--- navigation/Tabs.js
+++ navigation/Tabs.js
@@ -1,19 +1,39 @@
 import React from "react";
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Moives from "../screens/Movies";
 import Tv from "../screens/Tv";
 import Search from "../screens/Search";
+import { View, Text } from "react-native-web";
 
 const Tab = createBottomTabNavigator();
 
 function Tabs() {
-    return (
-      <Tab.Navigator>
-        <Tab.Screen name="Movies" component={Moives} />
-        <Tab.Screen name="Tv" component={Tv} />
-        <Tab.Screen name="Search!@!@" component={Search} />
-      </Tab.Navigator>
-    );
-  }
+  return (
+    <Tab.Navigator
+      initialRouteName="Tv"
+      screenOptions={{
+        tabBarLabelPosition: "beside-icon",
+        tabBarActiveTintColor: "red",
+        tabBarInactiveTintColor: "purple",
+      }}
+    >
+      <Tab.Screen name="Movies" component={Moives} />
+      <Tab.Screen
+        name="Tv"
+        component={Tv}
+        options={{
+          tabBarBadge: 5,
+          headerTitleStyle: { color: "tomato" },
+          headerRight: () => {
+            <View>
+              <Text>Hellodwdwqd</Text>
+            </View>
+          },
+        }}
+      />
+      <Tab.Screen name="Search!@!@" component={Search} />
+    </Tab.Navigator>
+  );
+}
 
-  export default Tabs;
+export default Tabs;
